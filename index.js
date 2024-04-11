4b38c8fa39169a81caae11778d96b858c0a8d719
Alice Veselá <alice.hladikova@gmail.com>
2024-04-11 11:53:49 +0200

    pridani js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Funguju')
+const kameny = document.querySelectorAll('.kamen');
+
+/*
+kameny.forEach((kamen) => {
+  kamen.addEventListener('click', (event) => {
+    document.querySelector('h1').innerHTML = event.target.textContent;
+  });
+});
+*/
+
+kameny.forEach((kamen) =>
+  kamen.addEventListener('click', (event) => {
+    const SelectedKamen = event.target.textContent;
+
+    const Heading = document.querySelector('h1');
+    Heading.innerHTML = SelectedKamen;
+
+    const zvuk = new Audio(`tony/${SelectedKamen}.mp3`);
+    zvuk.play();
+  }),
+);
